7784007baae4029b2eae033af5659209eda51f39
Imotytskiy <Imotytskiy@gmail.com>
2024-06-26 13:41:46 +0300

    add local storage for lang

--- js/translate.js
+++ js/translate.js
@@ -9,11 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const updateTextContent = () => {
         const lang = langSelect.value;
+        localStorage.setItem('language', lang);
         portfolio.textContent = langdata.languages[lang].strings['portfolio'];
         about.textContent = langdata.languages[lang].strings['about'];
         contact.textContent = langdata.languages[lang].strings['contact'];
         price.textContent = langdata.languages[lang].strings['price'];
+        console.log("Language set to local: " , localStorage.getItem('language'));
     };
+
+
 
     langSelect.addEventListener('change', updateTextContent);
     updateTextContent(); // Initial call to set the correct language on load
