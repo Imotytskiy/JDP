daf209e9e75a206d51438155abec8410a5421551
Imotytskiy <Imotytskiy@gmail.com>
2024-06-26 17:46:39 +0300

    add lang

--- js/translate.js
+++ js/translate.js
@@ -14,11 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
         about.textContent = langdata.languages[lang].strings['about'];
         contact.textContent = langdata.languages[lang].strings['contact'];
         price.textContent = langdata.languages[lang].strings['price'];
-        console.log("Language set to local: " , localStorage.getItem('language'));
+        console.log("Language set to local:", localStorage.getItem('language'));
     };
 
+    // Check if a language is stored in localStorage and set the dropdown value accordingly
+    if (localStorage.getItem('language')) {
+        langSelect.value = localStorage.getItem('language');
+    } else {
+        langSelect.value = langSelect.value; // Use the default value from the dropdown
+    }
 
+    // Initial call to set the correct language on load
+    updateTextContent();
 
+    // Add the event listener to the dropdown
     langSelect.addEventListener('change', updateTextContent);
-    updateTextContent(); // Initial call to set the correct language on load
 });
