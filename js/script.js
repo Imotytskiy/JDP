484de9fce305746ca2c3cbbee429b382c7cf7b2a
Igor Motytskiy <imotytskiy@gmail.com>
2024-07-14 13:30:20 -0700

    add

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow: '<button type="button" class="slick-prev" style="position: absolute; top: 50%; transform: translate(-50%); left: +55px; border:none; background-color:#424B5ACC;"><img style="width: 48px; heigh: 48px; background-color:#424B5ACC;" src="../images/icons/Chevron_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"style="position: absolute; top: 50%; transform: translate(-50%); left: +1000px; border:none;padding: 0;"><img style=" background-color:#424B5ACC;" src="../images/slides/arrownext.png"></button>',
-
-    });
+$(document).ready(function () {
+  $(".your-class").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow:
+      '<button type="button" class="slick-prev" style="position: absolute; top: 50%; transform: translateY(-50%); left: 10px; border: none; padding: 0; z-index: 1;"><img style="width: 48px; height: 48px; background-color:#424B5ACC;" src="../images/icons/Chevron_left.svg" alt="Previous"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next" style="position: absolute; top: 50%; transform: translateY(-50%); right: 10px; border: none; padding: 0; z-index: 1;"><img style="width: 48px; height: 48px; background-color:#424B5ACC;" src="../images/slides/arrownext.png" alt="Next"></button>',
   });
+});
